4a93ee9b7729947c61e2ca15d3e9d3f6bccf78e2
udalmia <utkarshdalmia3@gmail.com>
2022-01-07 19:03:28 +0530

    files

---
+++ src/utils/helperList.js
@@ -0,0 +1,100 @@
+export const showcaseSkills = [
+    {
+      label: "Javascript",
+      rating: 71,
+    },
+    {
+      label: "Node JS",
+      rating: 81,
+    },
+    {
+      label: "React",
+      rating: 76,
+    },
+    {
+      label: "Mongo dB",
+      rating: 72,
+    },
+    {
+      label: "Redux",
+      rating: 73,
+    },
+    {
+      label: "Express",
+      rating: 74,
+    },
+    {
+      label: "HTML CSS",
+      rating: 85,
+    },
+  ];
+  
+  export const frontSkillsList = [
+    {
+      label: "HTML5",
+      src: "https://img.icons8.com/color/480/000000/html-5--v1.png",
+    },
+    {
+      label: "CSS3",
+      src: "https://img.icons8.com/color/480/000000/css3.png",
+    },
+    {
+      label: "Javascript",
+      src: "https://img.icons8.com/color/480/000000/javascript.png",
+    },
+    {
+      label: "React",
+      src: "https://img.icons8.com/color/480/000000/react-native.png",
+    },
+    {
+      label: "Redux",
+      src: "https://img.icons8.com/color/480/000000/redux.png",
+    },
+    {
+      label: "Bootstrap",
+      src: "https://i.ibb.co/zx0Tt9M/bootstrap.png",
+    },
+    {
+      label: "Github",
+      src: "https://img.icons8.com/fluent/480/000000/github.png",
+    },
+    {
+      label: "NPM",
+      src: "https://img.icons8.com/color/480/000000/npm.png",
+    }
+    
+  ];
+  
+  export const backSkillsList = [
+    {
+      label: "Node JS",
+      src: "https://img.icons8.com/color/480/000000/nodejs.png",
+    },
+    {
+      label: "Express JS",
+      src: "https://i.ibb.co/9smDM6j/pngegg.png",
+    },
+    {
+      label: "Mongo dB",
+      src: "https://i.ibb.co/ZVh01dm/rsz-mong2.png",
+    },
+  ];
+  
+  export const projectsList = [
+    {
+      label: "Interntheory",
+      imgSrc: "images/s.jpg",
+      src: "https://intern-theory-clone.herokuapp.com/InternTheory",
+      githubSrc: "https://github.com/RoushanFW196/Intern-Theory-clone-Ejs",
+      description: "The website is a clone of Interntheory.com for online internships and courses",
+      stack: ["MongoDB","Express", "javascript", "HTML", "CSS",],
+    },
+    {
+      label: "PluralSight",
+      imgSrc: "images/s2.jpg",
+      src: "https://github.com/Udalmia/Project/tree/master/main",
+      githubSrc: "https://github.com/Udalmia/Project/tree/master/main",
+      description: "The website is a clone of PluralSight.com for online internships and courses and certifications",
+      stack: ["MongoDB","Express", "javascript", "HTML", "CSS",],
+    },
+  ];
